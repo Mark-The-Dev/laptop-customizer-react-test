b562c87ed67a59b32a4a88a41caab0ac381503d3
Mark Marcello <SwiftTactics@gmail.com>
2020-08-28 17:15:49 -0400

    removed build ignore

---
+++ precache-manifest.241dcd6a8a1ad6fa1def45adda9f5c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0210c6b42e8a4caa9e243d7dfb9c20cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46e9b4c10916009cab2",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "a46e9b4c10916009cab2",
+    "url": "/static/js/main.d56699b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
